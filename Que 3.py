9ea8b78bdd14ff4d7761977d2cebb57a3f2925df
Abantika  Jena <68710388+AbantikaJena@users.noreply.github.com>
2020-10-14 00:24:24 +0530

    Que 3

--- Que 3.py
+++ Que 3.py
@@ -4,20 +4,22 @@
 
 # Most commonly occurring word in the text file
 
-name = input('Enter file:')
-handle = open(name, 'r')
-counts = dict()
+def common_word():
+    name = input('Enter file:')
+    file = open(name, 'r')
+    counts = dict()
+    for line in file:
+        words = line.split()
+        for word in words:
+            word_l = word.lower()
+            counts[word_l] = counts.get(word_l, 0) + 1
 
-for line in handle:
-    words = line.split()
-    for word in words:
-        counts[word] = counts.get(word, 0) + 1
+    big_count = None
+    big_word = None
+    for word, count in list(counts.items()):
+        if big_count is None or count > big_count:
+            big_word = word
+            big_count = count
 
-bigcount = None
-bigword = None
-for word, count in list(counts.items()):
-    if bigcount is None or count > bigcount:
-        bigword = word
-        bigcount = count
-
-print(bigword, " appeared ", bigcount, "times.")
+    print(big_word, " appeared ", big_count, "times.")
+common_word()
